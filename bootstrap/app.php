31ef34f98ce853b307b9e40b08585f626f4f4013
HaikalFadhilah <110004043+haikalfadhilah05@users.noreply.github.com>
2026-05-25 19:52:47 +0700

    fix api auth redirect

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,12 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => null
         );
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        //
+    ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->render(function (AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Unauthenticated.'
+                ], 401);
+            }
+        });
     })->create();
